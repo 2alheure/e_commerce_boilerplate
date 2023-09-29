7a562d909238e839673a19a0afaea8d1c512f25b
2alheure <jojo77.juv@live.fr>
2023-09-29 15:35:21 +0200

    first commit

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Commande;
+use App\Entity\Produit;
+use App\Entity\Status;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController {
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response {
+        return $this->render('admin/home.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard {
+        return Dashboard::new()
+            ->setTitle('E Commerce');
+    }
+
+    public function configureMenuItems(): iterable {
+        yield MenuItem::linkToDashboard('Accueil', 'fa fa-home');
+        yield MenuItem::linkToCrud('Les utilisateurs', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Les commandes', 'fas fa-file-invoice', Commande::class);
+        yield MenuItem::linkToCrud('Les produits', 'fas fa-cart-shopping', Produit::class);
+        yield MenuItem::linkToCrud('Les status', 'fas fa-tag', Status::class);
+    }
+}
