2d7bc8497e87a8ff2fccdbc342b224bdcacf38ff
2alheure <jojo77.juv@live.fr>
2023-09-29 16:18:53 +0200

    Product CRUD update

--- src/Controller/Admin/ProduitCrudController.php
+++ src/Controller/Admin/ProduitCrudController.php
@@ -3,23 +3,29 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Produit;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 
-class ProduitCrudController extends AbstractCrudController
-{
-    public static function getEntityFqcn(): string
-    {
+class ProduitCrudController extends AbstractCrudController {
+    public static function getEntityFqcn(): string {
         return Produit::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
-    {
+    public function configureFields(string $pageName): iterable {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            TextField::new('nom'),
+            MoneyField::new('prix')->setCurrency('EUR'),
             TextEditorField::new('description'),
+            ImageField::new('image')
+                ->setBasePath('assets/uploads/products')
+                ->setUploadDir('public/assets/uploads/products')
+                ->setUploadedFileNamePattern('[uuid].[extension]'),
+            IntegerField::new('stock'),
         ];
     }
-    */
 }
